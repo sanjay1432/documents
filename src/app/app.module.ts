5b7dc67d0882000e322040d6874763fdb303a68f
Sanjay <sksanjaychopra8@gmail.com>
2018-04-25 21:57:53 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from "@angular/common/http";
 import { AppComponent } from './app.component';
 import { DocumentsService } from './documents.service';
-
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,HttpModule,HttpClientModule
+    BrowserModule,HttpModule,HttpClientModule,PdfViewerModule
   ],
   providers: [DocumentsService,HttpClient],
   bootstrap: [AppComponent]
